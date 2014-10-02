1df2e92b41aede88264bf59f7d9bd9831c26f125
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-10-02 01:24:03 -0500

    Check to make sure input file contains a valid path.

--- symlink-toggle.sh
+++ symlink-toggle.sh
@@ -9,21 +9,28 @@ set -e
 
 # If the input file exists and is a symbolic link,
 # we want to turn it into a text file with the same name.
-if [ -L $* ]
-then
+if [ -L $* ]; then
   TARGET=$(readlink --canonicalize-existing $*)
   rm -- $*
   echo "$TARGET" > $*
-elif [ -f $* -a -s $* ]
-then
+# However, if the input is a regular file with a non-zero size,
+# we want to turn it into a symbolic link to wherever its contents point.
+elif [ -f $* -a -s $* ]; then
   TARGET=$(<$*)
-  rm -- $*
-  ln --symbolic $TARGET "$*" 
+  # Check to see if the file contents are a valid path.
+  if [ -e $TARGET ]; then
+    rm -- $*
+    ln --symbolic $TARGET "$*"
+  else
+    echo "\`$TARGET\` is not a valid target for a symbolic link."
+    exit 2
+  fi
 else
-  echo "`$*` is not an symbolic link or regular file."
+  echo "\`$*\` is not an symbolic link or regular file."
+  exit 1
 fi
 
-#TODO: check for broken symbolic links.
+#TODO: check for broken symbolic links. Or maybe they should just be transformed normally?
 #TODO: check permissions.
 #TODO: preserve permissions?
 #TODO: preserve modification time?
