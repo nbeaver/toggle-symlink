e16061cd98f8ee92520d858bbfc0523e79de33cb
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-10-02 20:01:10 -0500

    Made test script so that it can be re-run without error.

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ mkdir -p example-symlinks/
 echo "This is not a valid symlink path." > example-symlinks/file.txt
 # Make relative links.
 cd example-symlinks/
-ln --symbolic file.txt 'relative link to file.txt'
-ln --symbolic file.txt '*'
-#ln --symbolic file.txt '--force *'
+ln --symbolic --force file.txt 'relative link to file.txt'
+ln --symbolic --force file.txt '*'
+#ln --symbolic --force file.txt '--force *'
 cd -
 # Make an abolute link.
-ln --symbolic "$PWD/example-symlinks/file.txt" "$PWD/example-symlinks/absolute link to file.txt"
+ln --symbolic --force "$PWD/example-symlinks/file.txt" "$PWD/example-symlinks/absolute link to file.txt"
 # Test the toggling capability of each one.
 cd example-symlinks/
 ../toggle-symlink.sh 'relative link to file.txt'
@@ -20,3 +20,4 @@ cd example-symlinks/
 #../toggle-symlink.sh '--force *'
 ../toggle-symlink.sh 'absolute link to file.txt'
 ../toggle-symlink.sh 'absolute link to file.txt'
+cd -
