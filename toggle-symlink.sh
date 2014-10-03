49ae3e231027a4ea5daf98e717391ff6c4ea5bd0
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-10-02 19:17:08 -0500

    Made shell script marginally safer.

--- toggle-symlink.sh
+++ toggle-symlink.sh
@@ -5,7 +5,10 @@
 set -o nounset
 
 # Terminate as soon as any command fails.
-set -e
+set -o errexit
+
+# Don't overwrite an existing file.
+set -o noclobber
 
 # If the input file exists and is a symbolic link,
 # we want to turn it into a text file with the same name.
@@ -20,7 +23,7 @@ elif [ -f "$*" -a -s "$*" ]; then
   # Check to see if the file contents are a valid path.
   if [ -e $TARGET ]; then
     rm -- "$*"
-    ln --symbolic $TARGET "$*"
+    ln --symbolic -- $TARGET "$*"
   else
     echo "\`$TARGET\` is not a valid target for a symbolic link."
     exit 2
@@ -30,6 +33,7 @@ else
   exit 1
 fi
 
+#
 #TODO: check for broken symbolic links. Or maybe they should just be transformed normally?
 #TODO: check permissions.
 #TODO: preserve permissions?
