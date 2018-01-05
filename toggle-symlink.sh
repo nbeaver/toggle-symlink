7e4dfa3dc660b5c1270cf5761dc1a245a415515f
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-01-04 18:12:43 -0600

    More idiomatic style.

--- toggle-symlink.sh
+++ toggle-symlink.sh
@@ -14,44 +14,55 @@ set -o noclobber
 symlink_to_txt () {
     local TARGET="$(readlink -- "$*")"
     rm -- "$*"
-    printf %s "$TARGET" > "$*"
+    printf '%s' "$TARGET" > "$*"
 }
 
 txt_to_symlink() {
     # Store file contents in the $TARGET variable.
     local TARGET="$(<"$*")"
     # Check to see if the file contents are a valid path.
-    if [ -e "$TARGET" ]; then
+    if test -e "$TARGET"
+    then
         rm -- "$*"
         ln --symbolic -- "$TARGET" "$*"
     else
-        echo "Warning: did not convert \`$file\` because \`$TARGET\` is not a valid target for a symbolic link."
+        printf 'Error: did not convert `%s` because `%s` is not a valid target for a symbolic link.' "$file" "$target"
         return 2
     fi
 }
 
 toggle_symlink() {
-    # If the input file exists and is a symbolic link (-L),
-    # we want to turn it into a text file with the same name.
-    if [ -L "$*" ]; then
+    if test -L "$*"
+    then
+        # If the input file exists and is a symbolic link (-L),
+        # we want to turn it into a text file with the same name.
         symlink_to_txt "$*"
+        return 0
+    elif test -f "$*"
+    then
         # However, if the input is a regular file (-f) with a non-zero size (-s),
         # we want to turn it into a symbolic link to wherever its contents point.
-    elif [ -f "$*" -a -s "$*" ]; then
         txt_to_symlink "$*"
+        return 0
     else
-        echo "Warning: did not convert \`$file\` because is not a symbolic link or regular file."
+        printf 'Error: did not convert `%s` because it is not a symbolic link or regular file.' "$*"
         return 1
     fi
 }
 
 # Show usage information if there are no arguments.
-if [ $# == 0 ] ; then
-    echo "Usage: toggle-symlink.sh command-name"
+if test $# -eq 0
+then
+    printf 'Usage: %s /path/to/file\n' "$0"
     exit 1;
 fi
 
 for file in "$@"
 do
-    toggle_symlink "$file"
+    if test -f "$file"
+    then
+        toggle_symlink "$file"
+    else
+        printf 'Warning: path does not exist: %s' "$file"
+    fi
 done
