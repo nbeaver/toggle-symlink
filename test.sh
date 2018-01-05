9a5b1ddfff74e27411676c3604d1b564fcd0e464
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-01-05 10:48:10 -0600

    Check the symlinks are actually the same.

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Treat unset variables and parameters
 # other than the special parameters "@" and "*"
 # as an error when performing parameter expansion.
@@ -18,15 +19,34 @@ ln -s -- 'relative link to file.txt' "$DIR/"'relative link to relative link to f
 # Make an abolute link.
 ln -s -- "$DIR/file.txt" "$DIR/absolute link to file.txt"
 
+function test_toggle {
+    local LINK="$*"
+    if ! test -L "$LINK"
+    then
+        printf 'Error: not a symbolic link: %s\n' "$LINK"
+        return 1
+    fi
+    local BEFORE="$(file "$LINK")"
+    ./toggle-symlink.sh "$LINK"
+    ./toggle-symlink.sh "$LINK"
+    local AFTER="$(file "$LINK")"
+    if ! test "$AFTER" = "$BEFORE"
+    then
+        printf 'Error: toggle-symlink.sh altered link.\n'
+        printf 'Before: %s\n' "$BEFORE"
+        printf 'After : %s\n' "$AFTER"
+        return 1
+    else
+        return 0
+    fi
+}
+
 # Test the toggling capability of each one.
-./toggle-symlink.sh "$DIR"/'relative link to file.txt'
-./toggle-symlink.sh "$DIR"/'relative link to file.txt'
+test_toggle "$DIR"/'relative link to file.txt'
 
-./toggle-symlink.sh "$DIR"/'absolute link to file.txt'
-./toggle-symlink.sh "$DIR"/'absolute link to file.txt'
+test_toggle "$DIR"/'absolute link to file.txt'
 
-./toggle-symlink.sh "$DIR"/'relative link to relative link to file.txt'
-./toggle-symlink.sh "$DIR"/'relative link to relative link to file.txt'
+test_toggle "$DIR"/'relative link to relative link to file.txt'
 
 # Toggle them all at once.
 ./toggle-symlink.sh \
